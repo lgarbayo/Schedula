52b1598aa63f147e79e07712ae74f01259209ad7
lgarbayo <lugarbayo@gmail.com>
2025-07-13 14:28:16 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <title>Schedula</title>
+
+        <meta name="description" content="The smart calendar that keeps your life organized—your way">
+    
+        <link rel="stylesheet" href="index.css"/>
+    </head>
+
+    <body onload="">
+
+        <header>
+            <h1>Schedula<br></h1>
+        </header>
+
+        <!--clock-->
+        <div class="clock">
+            <div id="clk"></div>
+        </div>
+
+        <!--calendar-->
+        <div class="calendar">
+            <div class="btn-calendar">
+                <button class="nav-btn">
+                    <img id="botonPREV" src="./icons/PREV.png" onclick="" />
+                </button>
+                <h2 id="monthYear" style="margin: 0"></h2>
+                <button class="nav-btn">
+                    <img id="botonNEXT" src="./icons/NEXT.png" onclick="" />
+                </button>
+            </div>
+
+            <div class="calendar-grid" id="calendar"></div>
+
+        </div>
+
+        <!--Modal for add/edit/delete/showcurrent/search appointment-->
+        <div class="modal" id="eventModal">
+            <div class="modal-content">
+
+                <!--Dropdown Selector-->
+                <div id="eventSelectorWrapper" style="display: none;">
+                    <label for="eventSelector">
+                        <strong>Select Event:</strong>
+                    </label>
+                    <select id="eventSelector" onchange="handleEventSelection(this.value)">
+                        <option disabled selected>
+                            Choose Event...
+                        </option>
+                    </select>
+                </div>
+
+                <!--Main Form-->
+                <form method="POST" id="eventForm">
+                    <input type="hidden" name="action" id="formAction" value="add">
+                    <input type="hidden" name="event_id" id="eventId">
+
+                    <label for="eventName">Event Name:</label>
+                    <input type="text" name="event_name" id="eventName" required>
+
+                    <label for="instructorName">Instructor Name:</label>
+                    <input type="text" name="instructor_name" id="instructorName" required>
+
+                    <label for="startDate">Start Date:</label>
+                    <input type="date" name="start_date" id="startDate" required>
+
+                    <label for="endDate">End Date:</label>
+                    <input type="date" name="end_date" id="endDate" required>
+
+                    <label for="startTime">Start Time:</label>
+                    <input type="time" name="start_time" id="startTime" required>
+
+                    <label for="endTime">End Time:</label>
+                    <input type="time" name="end_time" id="endTime" required>
+
+                    <label for="descriptionEvent">End Time:</label>
+                    <input type="descript" name="descript_event" id="descriptionEvent" required>
+
+                    <button type="submit">Save</button>
+                </form>
+
+                <!--Delete-->
+                <form method="POST" onsubmit="return confirm('Are you sure you want to delete this appointment?')">
+                    <input type="hidden" name="action" value="delete">
+                    <input type="hidden" name="event_id" id="deleteEventId">
+                    <button type="submit" class="submit-btn">Delete</button>
+                </form>
+
+                <!--Cancel-->
+                <button type="button" class="submit-btn" onclick="closeModal()" style="background:#ccc">Cancel</button>
+            </div>
+        </div>
+
+
+    </body>
+
+
+</html>
